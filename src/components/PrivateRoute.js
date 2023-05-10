940abfc3cbf4f52138590cfef3cf54c6d5f454b3
Yaroslav <110727630+BaristaYaroslv@users.noreply.github.com>
2023-05-10 22:01:12 +0300

    update route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,6 +4,9 @@ import useAuth from 'hooks/useAuth';
 const PrivateRoute = ({ component: Component, redirectTo = '/login' }) => {
   const { isLoggedIn, isRefresing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefresing;
+  console.log(isLoggedIn); //false
+  console.log(isRefresing); //false
+  console.log(shouldRedirect); //ture
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
